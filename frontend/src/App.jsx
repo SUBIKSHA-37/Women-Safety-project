6dcf540a3d317aa867e737d473b27b968ed17341
SUBIKSHA-37 <iamsubikshalakshmi@gmail.com>
2026-09-23 08:16:54 +0530

    the appp sstuff

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ import Loader from './components/Loader';
 import RouteOptions from './components/RouteOptions';
 
 // ✅ FIXED API URL (important)
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = "https://nila-backend-production-db13.up.railway.app";
 
 // distance calculation
 const distance = (a, b) => {
